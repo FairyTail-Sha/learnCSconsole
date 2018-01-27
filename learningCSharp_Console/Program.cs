0fbd9227bb8d57ee3a8161959cde2d205d1db9d5
Sharif-D <3188208752@qq.com>
2018-01-27 18:48:44 +0800

    第六章函数完成，函数结构，参数，结构函数，Main函数，委托。

--- learningCSharp_Console/Program.cs
+++ learningCSharp_Console/Program.cs
@@ -1,15 +1,177 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿#region Using 
+using System;
+using System.Windows.Input;
+using static System.Console;
+using static System.Double;
+using static System.String;
+using static System.Double;
+using TimeTable;
+#endregion
 
-namespace learningCSharp_Console
+#region 主程序
+namespace Csharp学习
 {
     class Program
     {
+        delegate double ProcessDelegate(double param1, double param2);
+        static double Multiply(double param1, double param2) => param1 * param2;
+        static double Divide(double param1, double param2) => param1 / param2;
+        struct CustomerName
+        {
+            public string firstName, lastName;
+            public string Name() => firstName + " " + lastName;
+        }
+        struct order
+        {
+            public string itemName;
+            public int unitCount;
+            public double unitCost;
+            public double GetSum()
+            {
+                return unitCount * unitCost;
+            }
+            public string Show()
+            {
+                return $"Order Information: {this.unitCount} " +
+                    $"{this.itemName} items at ${this.unitCost} each, " +
+                    $"total cost ${this.GetSum()}";
+            }
+        }
         static void Main(string[] args)
         {
+            #region 数组测试
+            // string[] friendNames = { "Qa Ws", "Ws Es", "Esa Rt" };
+            // int [][] jag = {new int[] {1,2,3},
+            //                 new int[] {2,3,4,5},
+            //                 new int[] {9,5,6,7,8}};
+            // Console.WriteLine($"Here are {friendNames.Length} of my friends!");
+            // for(int i = 0; i<friendNames.Length;i++)
+            // {
+            //     Console.WriteLine(friendNames[i]);
+            // }
+            // foreach(string s in friendNames)
+            // {
+            //     Console.WriteLine(s);
+            // }
+            #endregion
+            #region 字符串测试
+
+            // string myString = "A string";
+            // char[] myChar = myString.ToCharArray();
+            // foreach(char character in myChar){
+            //     Console.WriteLine($"{character}");
+            // }
+
+            // string myString = Console.ReadLine();
+            // WriteLine($"You typed {myString.Length} characters.");
+
+            // string userResponse = ReadLine();
+            // char[] trimChars = {' ', 'e','s'};
+            // //删除字符串前后的空格 
+            // userResponse = userResponse.Trim(trimChars: trimChars);
+            // if(userResponse.ToLower() == "y")
+            // {
+            //     WriteLine($"YES!");
+            // }
+
+            // string myString = "Aligned";
+            // WriteLine($"You typed {myString.Length} characters.");
+            // myString = myString.PadRight(10,'+');
+            // WriteLine($"myString has {myString.Length} characters.");
+            // WriteLine($"myString is {myString}");
+
+            // string myString = "This is a test.";
+            // char[] separator = {' '};
+            // string[] myWords;
+            // myWords = myString.Split(separator);
+            // foreach(string word in myWords){
+            //     WriteLine($"*{word}*");
+            // }
+            // ReadKey();
+
+            #endregion
+            #region 日历测试
+            // MyTime myTime = new MyTime(2018,1,20);
+            //WriteLine($"2017,10,21是{myTime.GetWeek()}");
+            // myTime.ShowCalender();
+            #endregion
+            #region 函数测试
+            // double n = double.Parse(ReadLine());
+            // double m = double.Parse(ReadLine());
+            // WriteLine($"{n} * {m} = {Product(n,m)}");
+            //WriteLine($"sum of (1~5) = {SumVals(1,2,3,4,5)}");
+            ///out输出参数
+            //SumVals(new int[]{1,2,3,4,5},out int sum);
+            //WriteLine($"sum of (1~5) = {sum}");
+            ///ref值参数
+            //SumVals(ref sum);
+            //WriteLine($"sum +sum of (1~5) = {sum}");
+            ///Main()函数
+            //WriteLine($"{args.Length} command line arguments were apecified:");
+            //foreach(string arg in args)
+            //{
+            //    WriteLine(arg);
+            //}
+            ///结构函数
+            //CustomerName myCustomer;
+            //myCustomer.firstName = "Jhon";
+            //myCustomer.lastName = "Franklin";
+            //WriteLine(myCustomer.Name());
+            //ReadKey();
+            ///委托 delegate
+            //ProcessDelegate process;
+            //WriteLine("Enter 2 number separated with a comma:");
+            //string input = ReadLine();
+            //int commaPos = input.IndexOf(',');
+            //double param1 = double.Parse(input.Substring(0, commaPos));
+            //double param2 = double.Parse(input.Substring(commaPos + 1, input.Length - commaPos - 1));
+            //WriteLine("Enter M to multiply or D to divide:");
+            //input = ReadLine();
+            //if(input=="M")
+            //{
+            //    process = new ProcessDelegate(Multiply);
+            //}
+            //else
+            //{
+            //    process = new ProcessDelegate(Divide);
+            //}
+            //WriteLine($"Result: {process(param1, param2)}");
+            //order myoder = new order();
+            //myoder.itemName = "螺丝";
+            //myoder.unitCost = 0.5;
+            //myoder.unitCount = 200;
+            //WriteLine(myoder.Show());
+            //ReadKey();
+            #endregion
+
+        }
+        static double Product(double number1,double number2)=>number1*number2;
+        static int SumVals(params int[] vals)
+        {
+            int sum = 0;
+            foreach(int val in vals)
+            {
+                sum += val;
+            }
+            return sum;
+        }
+        static void SumVals(int[] vals,out int sum)
+        {
+            sum = 0;
+            foreach(int val in vals)
+            {
+                sum += val;
+            }
+        }
+        static void SumVals(ref int sum)
+        {
+            int[] vals = new int[]{1,2,3,4,5};
+            foreach(int val in vals)
+            {
+                sum += val;
+            }
         }
     }
+
 }
+#endregion
---
+++ learningCSharp_Console/Time.cs
@@ -0,0 +1,146 @@
+using System;
+
+namespace TimeTable
+{
+    class MyTime
+    {
+        public readonly int Year;
+        public readonly int Mouth;
+        public readonly int Days;
+
+        //月份天数
+        int[,] mouth =new int[2,12] {
+            {1 , 2, 3, 4, 5, 6, 7, 8, 9,10,11,12},
+            {31, 0,31,30,31,20,31,30,31,31,30,31}
+        };
+        //构造函数
+        public MyTime(int _year,int _mouth,int _day)
+        {
+            this.Year = _year;
+            this.Mouth = _mouth;
+            this.Days = _day;
+            //二月的天数
+            if(IsLeap(_year)){
+                mouth[1,1] = 29;
+            }else{
+                mouth[1,2-1] = 28;
+            }
+
+        }
+        //星期
+        enum Week : byte
+        {
+            Monday = 1,//Monday
+            Tuesday = 2,//Tuesday
+            Wednesday = 3,//Wednesday
+            Thursday = 4,//Thursday
+            Friday = 5,//Friday
+            Saturday = 6,//Saturday
+            Sunday = 0,//Sunday
+        }
+        #region 星期
+
+        private Week week;
+
+        public string GetWeek(){
+            SetWeek();
+            return this.week.ToString();
+        }
+        private void SetWeek(){
+            this.week = Zeller();
+        }
+        private Week Zeller()
+        {
+            int y,c,m,d;
+            if(this.Mouth < 3)
+            {
+                y = (this.Year - 1) % 100;
+                c = (this.Year - 1)/ 100;
+                m = this.Mouth + 12;
+                d = this.Days;
+            }else{
+                y = this.Year % 100;
+                c = this.Year / 100;
+                m = this.Mouth;
+                d = this.Days;
+            }
+
+            int w=y+y/4+c/4-2*c+26*(m+1)/10+d-1;
+            // Console.WriteLine($"y = {y}; c = {c}; m = {m}; d = {d}; week = {w%7}");
+            switch(w%7)
+            {
+                case 0:return Week.Sunday;
+                case 1:return Week.Monday;
+                case 2:return Week.Tuesday;
+                case 3:return Week.Wednesday;
+                case 4:return Week.Thursday;
+                case 5:return Week.Friday;
+                default:
+                return Week.Saturday;
+            }
+        }
+        #endregion
+        //闰年二月天数
+        public bool IsLeap(int year){
+            if((year%100 != 0 && year%4 == 0) || (year%400 == 0))
+            return true;
+            else return false;
+        }
+        //小日历
+        public void ShowCalender(){
+
+            int k = 15;
+            MyTime Start,End,Mid;
+            string big  = "big";
+            string small = "small";
+            bool isBig = true;
+            Start = this;
+            Mid = this;
+            while(k!=0)
+            {
+                k--;
+                //计算明天
+                Mid = NextDay(Mid);
+                if(k==14)
+                {
+                    //记录第一天
+                    Start = Mid ;
+                }
+                else if(k==0)
+                {
+                    //记录最后一天
+                    End = Mid;
+                    //输出
+                    string s = isBig?big:small;
+                    Console.WriteLine($"{s.PadRight(5)} 开始于{Start.Year}年{Start.Mouth.ToString().PadLeft(2)}月{Start.Days.ToString().PadLeft(2)}日"
+                        +$"~结束于{End.Year}年{End.Mouth.ToString().PadLeft(2)}月{End.Days.ToString().PadLeft(2)}日");
+                    //重新初始化
+                    isBig = !isBig;
+                    k = 15;
+                }
+
+                if(Mid.Year == 2019)
+                break;
+            }
+        }
+
+        private MyTime NextDay(MyTime taday)
+        {
+            if(taday.Days+1>taday.mouth[1,taday.Mouth-1])
+            {
+                if(taday.Mouth + 1 > 12)
+                {
+                    return new MyTime(taday.Year + 1,1,1);
+                }else
+                {
+                    return new MyTime(taday.Year,taday.Mouth + 1,1);
+                }
+            }else{
+                return new MyTime(taday.Year,taday.Mouth,taday.Days + 1);
+            }
+
+            
+        }
+
+    }
+}
